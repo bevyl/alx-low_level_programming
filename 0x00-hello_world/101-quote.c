af07b75f016333c695181ebf07b4fe0b85020895
bevyl <beverlynejahenda@gmail.com>
2023-06-16 08:12:19 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<unistd.h>
+
 /**
- * main - A c program that prints a line to the standard error
- * Return 1 (Success)
+ * main - Entry point 
+ *
+ * Description: print a quote using write function
+ *    ssize_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: 1 (error).
  */
 
 int main(void)
-{
-	fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
-
